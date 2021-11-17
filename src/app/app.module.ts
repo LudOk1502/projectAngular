8f2b59f339d748826baaae2952ab6e7af3ae9352
LudOk1502 <lyudmyla152@gmail.com>
2021-11-17 20:19:53 +0200

    classwork_lesson2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
 import {HttpClientModule} from "@angular/common/http";
-import { UsersComponent } from './components/users/users.component';
+import {UsersComponent} from './components/users/users.component';
 import {UserComponent} from "./components/user/user.component";
-import { PostsComponent } from './components/posts/posts.component';
-import { PostComponent } from './components/post/post.component';
+import {PostsComponent} from './components/posts/posts.component';
+import {PostComponent} from './components/post/post.component';
+import {RouterModule} from "@angular/router";
+import {UserDetailsComponent} from './components/user-details/user-details.component';
+import {PostDetailsComponent} from './components/post-details/post-details.component';
 
 @NgModule({
   declarations: [
@@ -14,11 +17,25 @@ import { PostComponent } from './components/post/post.component';
     UserComponent,
     UsersComponent,
     PostsComponent,
-    PostComponent
+    PostComponent,
+    UserDetailsComponent,
+    PostDetailsComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    RouterModule.forRoot([
+      {
+        path: 'users',
+        component: UsersComponent,
+        children: [{path: ':id', component: UserDetailsComponent}]
+      },
+      {
+        path: 'posts',
+        component: PostsComponent,
+        children: [{path: ':id', component: PostDetailsComponent}]
+      },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
